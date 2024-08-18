024e93e08487734f575b58cddf9b0ae65ebd6b87
Kevin <kevingomes793@gmail.com>
2024-08-18 18:42:37 -0300

    maioria das operações já estão funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,26 @@ function calcular(tipo,valor){
 
     /*verifica se é ação ou valor*/
     if(tipo === 'acao'){
-
+        // botão de apagar
+        if(valor === 'c'){
+            //limpar o visor
+            document.getElementById('resultado').value = ''           
+        }
+        //concatenando, junto com as operações
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+            document.getElementById('resultado').value += valor
+        }
+        //botão de igual
+        if(valor === '='){
+            var valor_campo = eval(document.getElementById('resultado').value)
+            //exibindo resultado da expressão
+            document.getElementById('resultado').value = valor_campo
+        }
     }else if(tipo === 'valor'){
         //se for valor, colocar no visor
         //seleionando objeto, acessando valor, atribuindo como valor o parametro valor
-        document.getElementById('resultado').value = valor
+        document.getElementById('resultado').value += valor
     }
+
+
 }
