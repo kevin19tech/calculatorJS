138565da84f8482cb5948c68da7dca155f7132ca
Kevin <kevingomes793@gmail.com>
2024-08-18 16:26:57 -0300

    Commit antes de voltar para a branchAlteracoesDesign

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 /*Funcão de calcular*/
 function calcular(tipo,valor){
-    console.log(tipo,valor)
+
+    /*verifica se é ação ou valor*/
+    if(tipo === 'acao'){
+
+    }else if(tipo === 'valor'){
+        //se for valor, colocar no visor
+        //seleionando objeto, acessando valor, atribuindo como valor o parametro valor
+        document.getElementById('resultado').value = valor
+    }
 }
